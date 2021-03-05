e7c96bc37351aeb66def9aa8bef33b7336dd7878
Tim301 <45065016+Tim301@users.noreply.github.com>
2021-03-05 17:49:56 +0100

    (Debug) role selector

--- Godot_Project/Lobby.gd
+++ Godot_Project/Lobby.gd
@@ -1,13 +1,22 @@
 extends Node2D
 
-
+onready var role = get_node("OptionButton")
+var selected_role = "loupgarou"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	role.add_item("loupgarou")
+	role.add_item("chaman")
+	role.add_item("villagoies")
 
 func _on_Pseudo_text_changed():
 	Global.Pseudo = get_node("Pseudo").text
 
 func _on_Validate_button_down():
+	Global.role = selected_role
 	Global.List_Joueurs.append(Global.Pseudo)
 	get_tree().change_scene("res://Bin/Scenes/Partie.tscn")
+
+
+func _role_selected(index):
+	print(role.get_item_text(index))
+	selected_role = role.get_item_text(index)
